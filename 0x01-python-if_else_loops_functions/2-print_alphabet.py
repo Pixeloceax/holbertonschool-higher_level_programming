029a5fee34bfd6d1fc8f35af9c601bbe050c2174
axel valentin <axel.valentinbvs@yahoo.com>
2021-12-21 11:53:19 +0000

    add task

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def lowercaseAlphabets():
-    for c in range(97, 123):
-        print("{}".format(chr(c)), end="")
+for c in range(97, 123):
+    print("{}".format(chr(c)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def lowercaseAlphabets():
-    for c in range(97, 123):
-        if chr(c) == "q" or chr(c) == "e":
-            continue
-        else:
-            print("{}".format(chr(c)), end="")
+for c in range(97, 123):
+    if chr(c) == "q" or chr(c) == "e":
+        continue
+    else:
+        print("{}".format(chr(c)), end="")
