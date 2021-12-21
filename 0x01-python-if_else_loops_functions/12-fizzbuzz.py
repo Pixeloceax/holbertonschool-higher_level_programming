b4c549ccd0a3d8380a940f707727f0318552d4ed
axel valentin <axel.valentinbvs@yahoo.com>
2021-12-21 16:09:45 +0000

    add task

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for fizzbuzz in range(101):
+        if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+            print("fizzbuzz", end=' ')
+            continue
+        elif fizzbuzz % 3 == 0:
+            print("fizz", end=' ')
+            continue
+        elif fizzbuzz % 5 == 0:
+            print("buzz", end=' ')
+            continue
+        print(fizzbuzz, end=' ')
