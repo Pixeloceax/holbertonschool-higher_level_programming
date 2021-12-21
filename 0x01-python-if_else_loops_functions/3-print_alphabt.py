43629436a6deac29ce7df6d0eb5eade18fffcde3
axel valentin <axel.valentinbvs@yahoo.com>
2021-12-21 11:37:31 +0000

    add task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def lowercaseAlphabets():
     for c in range(97, 123):
-        if c in [101, 113]:
+        if chr(c) == "q" or chr(c) == "e":
             continue
-        print(chr(c), end="")
-
-
-lowercaseAlphabets()
+        else:
+            print("{}".format(chr(c)), end="")
