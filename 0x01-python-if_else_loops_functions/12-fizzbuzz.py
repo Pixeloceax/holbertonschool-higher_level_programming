e45485b089bdc81e0b656e765ebe35a8e7de16c5
axel valentin <axel.valentinbvs@yahoo.com>
2021-12-21 16:26:07 +0000

    add task

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-def fizzbuzz():
+def fizzbuzz(): 
     for fizzbuzz in range(1, 101):
         if fizzbuzz % 15 == 0:
-            print("fizzbuzz", end=' ')
-            continue
+            print("FizzBuzz", end=" ")
         elif fizzbuzz % 3 == 0:
-            print("fizz", end=' ')
-            continue
+            print("Fizz", end=" ")
         elif fizzbuzz % 5 == 0:
-            print("buzz", end=' ')
-            continue
-        print(fizzbuzz, end=' ')
+            print("Buzz", end=" ")
+        else:
+            print(fizzbuzz, end=" ")
