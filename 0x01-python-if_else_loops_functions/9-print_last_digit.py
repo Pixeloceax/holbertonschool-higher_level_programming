99ca81a2bd50b95b8159b1c2928de6f6726e9833
axel valentin <axel.valentinbvs@yahoo.com>
2021-12-21 13:37:47 +0000

    add task

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    ldigit=number%10
-    print("{}".format(ldigit))
+        if number < 0:
+            number = -number
+        last_digit = (number % 10)
+        print("{}".format(last_digit), end='')
+        return last_digit
