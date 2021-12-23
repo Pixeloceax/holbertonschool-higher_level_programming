d8a49b1ca038213723eb898891202392ac444e9f
axel valentin <axel.valentinbvs@yahoo.com>
2021-12-23 12:56:59 +0000

    add task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, div , mul
     import sys
 
     sum = 0
 
+    list = ["+", "-", "*", "/"]
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if sys.argv[2] not in ['+', '-', '*', '/']:
+    if sys.argv[2] not in list:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         if sys.argv[2] == "+":
-            sum = a + b
+            sum = add(a, b)
         elif sys.argv[2] == "-":
-            sum = a - b
+            sum = sub(a, b)
         elif sys.argv[2] == "*":
-            sum = a * b
+            sum = mul(a, b)
         elif sys.argv[2] == "/":
-            sum = a / b
-    print("{} {} {} = {}".format(a, sys.argv[2], b, sum))
+            sum = div(a, b)
+    print("{0} {1} {2} = {3}".format(a, sys.argv[2], b, sum))
