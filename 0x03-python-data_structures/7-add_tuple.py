36504ec23ab31093c4bedc2167239c2d6ea73344
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-03 12:56:32 +0000

    add task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-        numbers1 = len(tuple_a)
-        numbers2 = len(tuple_b)
-        if numbers1 == 0 and numbers2 == 0:
-            tuple = (0, 0)
-        elif numbers1 == 1 and numbers2 == 1:
-            tuple = (tuple_a[0] + tuple_b[0], 0)
-        elif numbers1 == 1 and numbers2 != 0:
-            tuple = (tuple_a[0] + tuple_b[0], tuple_b[1])
-        elif numbers2 == 0 and numbers1 != 0:
-            tuple = (tuple_a[0], tuple_a[1])
-        elif numbers2 == 1 and numbers1 != 0:
-            tuple = (tuple_b[0] + tuple_a[0], tuple_a[1])
-        elif numbers1 == 0 and numbers2 != 0:
-            tuple = (tuple_b[0], tuple_b[1])
-        else:
-            tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return tuple
-
+    numbers1 = len(tuple_a)
+    numbers2 = len(tuple_b)
+    if numbers1 == 0 and numbers2 == 0:
+        tuple = (0, 0)
+    elif numbers1 == 1 and numbers2 == 1:
+        tuple = (tuple_a[0] + tuple_b[0], 0)
+    elif numbers1 == 1 and numbers2 != 0:
+        tuple = (tuple_a[0] + tuple_b[0], tuple_b[1])
+    elif numbers2 == 0 and numbers1 != 0:
+        tuple = (tuple_a[0], tuple_a[1])
+    elif numbers2 == 1 and numbers1 != 0:
+        tuple = (tuple_b[0] + tuple_a[0], tuple_a[1])
+    elif numbers1 == 0 and numbers2 != 0:
+        tuple = (tuple_b[0], tuple_b[1])
+    else:
+        tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return tuple
