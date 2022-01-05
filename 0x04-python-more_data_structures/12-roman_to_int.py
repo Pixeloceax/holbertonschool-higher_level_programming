640cf332f7428e063c93ddb15695e4cf8705344e
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-05 14:34:03 +0000

    add task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,6 @@ def roman_to_int(roman_string):
             num+=roman_string[s[i:i+2]]
             i+=2
         else:
-            #print(i)
             num+=roman_string[s[i]]
             i+=1
     return num
