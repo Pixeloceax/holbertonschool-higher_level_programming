4b1a9a573695fdcd8ab7333122fc11ab31804643
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-05 14:37:50 +0000

    add task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == []:
+    if a_dictionary:
+        max_key = max(a_dictionary, key=a_dictionary.get)
+    else:
         return None
-    max_key = max(a_dictionary, key=a_dictionary.get)
-    if max_key in a_dictionary:
-        return max_key
+    return max_key
