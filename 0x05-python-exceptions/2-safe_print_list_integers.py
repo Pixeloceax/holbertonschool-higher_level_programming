3052460e8e171ee38b10bb4954c57f075256f26e
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-17 11:30:23 +0000

    betty friendly

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     while True:
         try:
             if y < x:
-                print("{:d}".format(my_list[y]), end='')       
+                print("{:d}".format(my_list[y]), end='')
                 y += 1
                 int_check += 1
             else:
