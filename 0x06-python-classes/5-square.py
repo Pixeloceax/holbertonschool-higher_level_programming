30f3fb82b8a83f6acb399b2595a2e78133ca0079
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-18 16:11:55 +0000

    betty friendly

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -38,7 +38,12 @@ class Square:
         self.__size = value
     """class comment"""
     def my_print(self):
-        
-        print("#", end="")
-        if self.__size < 0:
+        """
+            self: instance of the class
+        """
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
             print()
