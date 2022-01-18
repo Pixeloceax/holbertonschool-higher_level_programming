65cececbe19e6a328c97819c175c26a411447de1
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-18 15:55:03 +0000

    betty friendly

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
 """Module comment"""
+
+
 class Square:
-    """class comment"""
-    def __init__(self, size = 0):
+    """class Square"""
+    def __init__(self, size=0):
+        """
+            self: instance of the class
+            size: size
+        """
         self.size = size
-        
     """class comment"""
     def area(self):
+        """
+            self: instance of the class
+        """
         return(self.__size**2)
-
     """class comment"""
     @property
     def size(self):
+        """
+            self: instance of the class
+        """
         return self.__size
 
     """class comment"""
     @size.setter
-    def size(self, value): 
-        if isinstance(value, int) == False:
+    def size(self, value):
+        """
+            self: instance of the class
+            value: value
+        """
+        if isinstance(value, int) is False:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
