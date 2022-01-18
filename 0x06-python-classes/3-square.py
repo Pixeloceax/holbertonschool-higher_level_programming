b16dafdaca894855a8ed0485bba01298fcbec75b
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-18 15:51:21 +0000

    betty friendly

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 """Module comment"""
-class Square:
-    """class comment"""
-    def __init__(self, size = 0):
 
-        if isinstance(size, int) == False:
+
+class Square:
+    """class Square"""
+    def __init__(self, size=0):
+        """
+            self: instance of the class
+            size: size
+        """
+        if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
     """class comment"""
     def area(self):
+        """
+            self: instance of the class
+        """
         return(self.__size**2)
