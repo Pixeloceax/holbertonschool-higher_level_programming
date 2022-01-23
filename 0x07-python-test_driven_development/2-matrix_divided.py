c2bfb8713f9c343d758388b4610bfb6002958abc
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-23 11:49:34 +0000

    add task

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" matrix_divided """
+def matrix_divided(matrix, div):
+    """
+        comment
+    """
+    size = 0
+    if not any(matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    for i in matrix:
+        if type(i) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for typeN in i:
+            if isinstance(typeN (int,float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if matrix[0] != None:
+        size = len(matrix[0])
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if isinstance(div (int,float)):
+        raise TypeError("div must be a number")
+    if len(i) != size:
+        raise TypeError("Each row of the matrix must have the same size")
+    return [[round(typeN / div, 2) for typeN in i] for i in matrix]
+    
+    
+    
+
+
+    
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""say_my_name"""
+def say_my_name(first_name, last_name=""):
+    
