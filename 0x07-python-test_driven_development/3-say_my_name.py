6b531023962280a72208fcb9619d9625c4418def
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-25 13:22:38 +0000

    add task

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 """say_my_name"""
+
+
 def say_my_name(first_name, last_name=""):
-    
+    """
+        comment
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {}".format(first_name), end='')
+    if len(last_name) == 0:
+        print()
+    else:
+        print(" {}".format(last_name))
