d97333551e7f148dd8751ff3aabc577609c50b6b
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-31 21:19:19 +0000

    import 9

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,56 +2,7 @@
 """comment"""
 
 
-from cgi import test
-
-
-class BaseGeometry():
-    """
-        comment
-    """
-    def area(self):
-        """
-            comment
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-            comment
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-"""comment"""
-
-
-class Rectangle(BaseGeometry):
-    """
-        comment
-    """
-    def __init__(self, width, height):
-        """
-            comment
-        """
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__height * self.__width
-
-    def __str__(self):
-        """
-            comment
-        """
-        return("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
-
-
-"""comment"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -62,17 +13,6 @@ class Square(Rectangle):
         """
             comment
         """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        super().integer_validator("size", size)
-
-    def area(self):
-        """
-            comment
-        """
-        return self.__size * self.__size
-
-    def __str__(self):
-        """
-            comment
-        """
-        return("[Rectangle] {:d}/{:d}".format(self.__size, self.__size))
