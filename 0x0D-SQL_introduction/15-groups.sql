58f8cbee9440bc76a842d3164ba91912cd61247b
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-03-08 15:32:42 +0000

    add task

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,5 @@
 -- Number by score
-SELECT SUM(`score`) 
-COUNT(*) AS `number`
+SELECT SUM(`score`), COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`
-ORDER BY `number` DESC
+ORDER BY `number` DESC;
