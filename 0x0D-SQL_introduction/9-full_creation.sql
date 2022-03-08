ce815f454d747a26996bd7b05849b77bef9c6c59
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-03-08 14:25:53 +0000

    add task

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,6 +4,3 @@ INSERT INTO `second_table`(id,name,score) VALUE(1, “John”, 10)
 INSERT INTO `second_table`(id,name,score) VALUE(2, “Alex”, 3)
 INSERT INTO `second_table`(id,name,score) VALUE(3, “Bob”, 14)
 INSERT INTO `second_table`(id,name,score) VALUE(4, “George”, 8)
-
-
-
