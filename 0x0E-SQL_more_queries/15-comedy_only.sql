c95f7016bc0eed446b30b771ddc3db8bdc6d027c
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-03-09 16:54:12 +0000

    add task

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,9 @@
 -- 
-SELECT tv_show.title
-FROM tv_show
-JOIN tv_show_genres 
-ON tv_show.id = tv_show_genres.show_id
-JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = Comedy
-ORDER BY tv_show.title;
+SELECT `tv_show.title`
+FROM `tv_show`
+JOIN `tv_show_genres` 
+ON `tv_show.id` = `tv_show_genres`.`show_id`
+JOIN `tv_genres`
+ON `tv_show_genres`.`genre_id` = `tv_genres.id`
+WHERE `tv_genres`.`name` = `Comedy`
+ORDER BY `tv_show`.`title`;
