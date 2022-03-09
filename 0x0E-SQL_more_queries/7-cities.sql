74ed9c10058aa25dc780ee9f3007aaa04cf35a44
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-03-09 14:47:50 +0000

    add task~

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
--- Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+-- Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `cities`(
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+    PRIMARY KEY(`id`),
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `state_id` INT NOT NULL,
     `name` VARCHAR(256) NOT NULL,
-    `states_id` INT NOT NULL,
-    FOREIGN KEY (states_id) REFERENCES states(id)
-);
+    FOREIGN KEY(`state_id`)
+    REFERENCES `hbtn_0d_usa`.`states`(`id`)
