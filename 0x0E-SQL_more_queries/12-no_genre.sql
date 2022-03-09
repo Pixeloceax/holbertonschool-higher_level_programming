72026a617a0d64e32b4dd036ce2751b773c584c0
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-03-09 16:31:19 +0000

    add task

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
--- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 11-genre_id_all_shows.sql)
+-- 
 SELECT t.`title`, g.`genre_id`
 FROM `tv_shows` AS t
 LEFT JOIN `tv_show_genres` AS g
-WHERE t.`id` = g.`show_id` IS NULL 
+ON t.`id` = g.`show_id`
+WHERE t.`id` = g.`show_id` IS NULL
 ORDER BY t.`title`, g.`genre_id`;
