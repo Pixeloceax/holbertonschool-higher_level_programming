3ba70f35055220250e2d6813235954eab0e8eee7
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-03-09 14:35:56 +0000

    add task~

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,8 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `cities`(
-    `id` INT NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(256) NOT NULL,
     `states_id` INT NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`states_id`) REFERENCES `states`(`id`)
+    FOREIGN KEY (states_id) REFERENCES states(id)
 );
