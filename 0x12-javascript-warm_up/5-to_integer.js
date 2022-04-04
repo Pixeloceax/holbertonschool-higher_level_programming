c4735a964c836d3e9516ed98875b6290d9865cc1
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-04 12:04:13 +0000

    add task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv.slice(2)[0]) {
-  console.log('My number:', parseInt(process.argv.slice(2)[0]));
-}
-if (isNaN(process.argv.slice(2)[0])) {
+if (isNaN(parseInt(process.argv.slice(2)[0]))) {
   console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(process.argv.slice(2)[0]));
 }
