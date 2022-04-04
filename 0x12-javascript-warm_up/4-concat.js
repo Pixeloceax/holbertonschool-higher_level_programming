8100d0ce44288b3c9d933c794b5f593027482373
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-04 10:29:06 +0000

    add task

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
 
+if (process.argv.slice(2)[0]) {
+  console.log(process.argv.slice(2)[0], 'is', process.argv.slice(2)[1]);
+} else if (process.argv.slice(2)[0]) {
+  console.log(process.argv.slice(2)[0], 'is');
+} else {
+  console.log(process.argv.slice(2)[0], 'is', process.argv.slice(2)[1]);
+}
