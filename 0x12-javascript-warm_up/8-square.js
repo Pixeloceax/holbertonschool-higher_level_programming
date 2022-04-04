44210dc20b1bbd56cfca629a458f06bbf9617c3a
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-04 14:01:05 +0000

    add task

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const n = process.argv.slice(2)[0];
+let string = '';
+
+if (isNaN(parseInt(n))) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      string += '*';
+    }
+    string += '\n';
+  }
+  console.log(string);
+}
