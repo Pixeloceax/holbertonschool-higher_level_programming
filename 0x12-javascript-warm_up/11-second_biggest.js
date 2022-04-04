b0647e77c53d9d22bf92043142854a68e158e81a
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-04 17:43:19 +0000

    add task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 if (process.argv.length <= 3) {
-    console.log(0);
-  } else {
-    const args = process.argv.map(Number).slice(2, process.argv.length).sort((a, b) => a - b);
-    console.log(process.argv.map(Number).slice(2, process.argv.length).sort((a, b) => a - b)[process.argv.map(Number).slice(2, process.argv.length).sort((a, b) => a - b).length - 2]);
-  }
+  console.log(0);
+} else {
+  process.argv.map(Number).slice(2, process.argv.length).sort((a, b) => a - b);
+  console.log(process.argv.map(Number).slice(2, process.argv.length).sort((a, b) => a - b)[process.argv.map(Number).slice(2, process.argv.length).sort((a, b) => a - b).length - 2]);
+}
