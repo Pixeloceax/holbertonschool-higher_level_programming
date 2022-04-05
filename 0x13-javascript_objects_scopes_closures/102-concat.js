bc5e763359dcb95c66add4b3f4a5508576d4fe3a
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-05 15:53:57 +0000

    add task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,9 +3,10 @@ const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-const fs = require('fs');
-const fileA_content = fs.readFileSync(fileA, 'utf8');
-const fileB_content = fs.readFileSync(fileB, 'utf8');
-const fileC_content = fs.readFileSync(fileC, 'utf8');
-console.log(fileA_content + fileB_content + fileC_content);
-
+if (!fileA || !fileB || !fileC) {
+  const fs = require('fs');
+  let file = '';
+  file += fs.readFileSync(fileA);
+  file += fs.readFileSync(fileB);
+  fs.writeFileSync(fileC, file);
+}
