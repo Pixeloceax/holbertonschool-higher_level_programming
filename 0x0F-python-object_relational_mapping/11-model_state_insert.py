21e9c661f531b9da86d63709c8a4574df7fbffb5
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-10 14:35:25 +0000

    add task

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    comment
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+import sys
+
+if __name__ == "__main__":
+    new_engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                               .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                               pool_pre_ping=True)
+    Session = sessionmaker(bind=new_engine)
+    session = Session()
+    name_State = State(name="Louisiana")
+    session.add(name_State)
+    session.commit()
+    print(name_State.id)
