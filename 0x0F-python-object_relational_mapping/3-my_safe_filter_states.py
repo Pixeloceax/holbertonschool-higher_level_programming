d8a6f152fffdfd8aef6f2c45d6d76d5beaef7e0a
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-10 13:44:50 +0000

    add task

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                          password=argv[2], database=argv[3])
+    cursor = con.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{:s}' ORDER BY id ASC"
+        .format(argv[4]))
+    db = cursor.fetchall()
+    for i in db:
+        print(i)
