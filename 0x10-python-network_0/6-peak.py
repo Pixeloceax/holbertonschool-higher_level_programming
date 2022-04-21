88bca69a11d3e8b42eb4315075ec3f48294358ad
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-21 19:18:09 +0000

    add task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,6 +13,10 @@ def find_peak(list_of_integers):
 
     if len(list_of_integers) == 1:
         return list_of_integers[0]
+    elif len(list_of_integers) == 2:
+        return ((list_of_integers[0], list_of_integers[1])
+                [list_of_integers[0] < list_of_integers[1]])
+
 
     if list_of_integers[int((len(list_of_integers) - 1) / 2)] < list_of_integers[int((len(list_of_integers) - 1) / 2) + 1]:
         return find_peak(list_of_integers[int((len(list_of_integers) - 1) / 2) + 1:])
