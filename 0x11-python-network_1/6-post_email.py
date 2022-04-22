229a41a4f4750db002a5ca9d537d99ed1fab96ee
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-22 21:17:54 +0000

    add standars style

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,8 @@ import requests
 import urllib.parse
 from sys import argv
 import urllib.request
+
 if __name__ == "__main__":
+
     email = requests.post(argv[1], data={'email': argv[2]})
     print(email.text)
