36055dc7902cf010a4a548fcde4173d8a0d73a15
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-22 13:17:38 +0000

    add task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    fetches holberton status
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
