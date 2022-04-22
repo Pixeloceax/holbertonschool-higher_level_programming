b16f94b795c4ecb58ea3df11abdabe28b3c7f9fb
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-04-22 21:15:55 +0000

    add task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    sends a POST request to the passed URL with the email as a parameter,
+"""
+
+import requests
+import urllib.parse
+from sys import argv
+import urllib.request
+if __name__ == "__main__":
+    email = requests.post(argv[1], data={'email': argv[2]})
+    print(email.text)
