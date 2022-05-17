e98b3d03f13a6af3dbb2b442c2c0e48fce64f1f2
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-05-17 13:17:32 +0000

    add task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,18 +3,18 @@
 const axios = require('axios');
 
 axios
-    .get(`${process.argv[2]}`)
-    .then((res) => {
-        let counter = 0;
-        for (const film of res.data.results) {
-            for (const listActors of film.characters) {
-                if (listActors.includes('people/18/')) {
-                    counter++;
-                }
-            }
+  .get(`${process.argv[2]}`)
+  .then((res) => {
+    let counter = 0;
+    for (const film of res.data.results) {
+      for (const listActors of film.characters) {
+        if (listActors.includes('people/18/')) {
+          counter++;
         }
-        console.log(counter);
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+      }
+    }
+    console.log(counter);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,16 +4,16 @@ const axios = require('axios');
 const fs = require('fs');
 
 axios
-    .get(process.argv[2])
-    .then((res) => {
-        if (res.status === 200) {
-            fs.writeFile(`./${process.argv[3]}`, res.data, { flag: 'w+' }, (err) => {
-                if (err) {
-                    console.error(err);
-                }
-            });
+  .get(process.argv[2])
+  .then((res) => {
+    if (res.status === 200) {
+      fs.writeFile(`./${process.argv[3]}`, res.data, { flag: 'w+' }, (err) => {
+        if (err) {
+          console.error(err);
         }
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+      });
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
